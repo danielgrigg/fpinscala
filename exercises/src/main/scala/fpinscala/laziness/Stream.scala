3fcf146c9d2c7e6dd874b41d6e7373a4d306545b
Daniel Casimir Grigg <daniel.grigg@skyfii.com>
2015-04-03 21:28:43 +1100

    streams

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -17,18 +17,45 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = sys.error("todo")
 
-  def drop(n: Int): Stream[A] = sys.error("todo")
+  def toList:List[A] = this match {
+    case Cons(h, t) => h()::(t().toList)
+    case _ => Nil
+  }
 
-  def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) => if (n > 0) Cons(h, () => t().take(n-1)) else empty
+    case _ => empty
+  }
 
-  def forAll(p: A => Boolean): Boolean = sys.error("todo")
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h, t) => if (n > 0) t().drop(n-1) else Cons(h,t)
+    case _ => this
+  }
 
-  def headOption: Option[A] = sys.error("todo")
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) => if (p(h())) Cons(h, () => t().takeWhile(p)) else empty
+    case _ => empty
+  }
+  def takeWhileUsingFoldRight(p: A => Boolean): Stream[A] =
+    foldRight(empty[A])((a,b) => if (p(a)) cons(a, b) else empty[A])
+    
+  def forAll(p: A => Boolean): Boolean = 
+    foldRight(true)((a,b) => p(a) && b)
+
+  def headOption: Option[A] = 
+    foldRight(None:Option[A])((a,b) => Some(a))
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
+
+  def map[B](f: A => B):Stream[B] = 
+    foldRight(empty[B])((a,b) => cons(f(a), b))
+
+  def filter(f: A => Boolean):Stream[A] = 
+    foldRight(empty[A])((a,b) => if (f(a)) cons(a, b) else b)
+
+//  def append(s2: => Stream[A]):Stream[A] = foldRight(s2)((a,b]) => cons(a,b))
 
   def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
 }
@@ -49,7 +76,15 @@ object Stream {
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
-  def from(n: Int): Stream[Int] = sys.error("todo")
+
+  def constant[A](a: A): Stream[A] = Stream.cons(a, constant(a))
+
+  def from(n: Int): Stream[Int] = Stream.cons(n, from(n + 1))
+
+  def fibsn(a:Int, b:Int):Stream[Int] = 
+    Stream.cons(a+b, fibsn(b, a+b))
+
+  val fibs:Stream[Int] = Stream.cons(0, Stream.cons(1, fibsn(0,1)))
 
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = sys.error("todo")
 }
