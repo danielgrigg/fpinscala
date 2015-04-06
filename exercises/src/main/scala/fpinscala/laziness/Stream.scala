1fe6dd4dfd382af3d307144f3e4cf0f446b2b68d
Daniel Grigg <mail@danielgrigg.com>
2015-04-06 12:37:12 +1000

    add chapter 5 exs to up 5.13

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -58,6 +58,39 @@ trait Stream[+A] {
 //  def append(s2: => Stream[A]):Stream[A] = foldRight(s2)((a,b]) => cons(a,b))
 
   def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
+
+  def mapViaUnfold[B](f: A => B):Stream[B] = 
+    unfold(this)(s => s match {
+      case Cons(x, xs) => Some((f(x()), xs()))
+      case empty => None
+    })
+
+  def takeViaUnfold(n: Int): Stream[A] = 
+    unfold((n,this))(s => s match {
+      case ((m,Cons(x, xs))) if m > 0  => Some(x(), (m-1, xs()))
+      case _ => None
+    })
+
+  def takeWhileViaUnfold(p: A => Boolean): Stream[A] = 
+    unfold(this)(s => s match {
+      case Cons(x, xs) if p(x()) => Some(x(), xs())
+      case _ => None
+    })
+
+  // contraviant nonsense
+  def zipWithViaUnfold[B,C](s2:Stream[B])(f:(A,B) => C):Stream[C] = 
+    unfold((this, s2))(s => s match {
+      case (Cons(x,xs), Cons(y,ys)) => Some((f(x(),y()), (xs(),ys())))
+      case _ => None
+    })
+
+    def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] = 
+      unfold((this, s2))(s => s match {
+        case (Empty, Empty) => None
+        case (Empty, Cons(y,ys)) => Some((None, Some(y())), (Empty, ys()))
+        case (Cons(x,xs), Empty) => Some((Some(x()), None), (xs(), Empty))
+        case (Cons(x,xs), Cons(y,ys)) => Some((Some(x()), Some(y())), (xs(), ys()))
+      })
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -86,5 +119,23 @@ object Stream {
 
   val fibs:Stream[Int] = Stream.cons(0, Stream.cons(1, fibsn(0,1)))
 
-  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = sys.error("todo")
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+    f(z) match {
+      case Some((value,state)) => Stream.cons(value, unfold(state)(f))
+      case None => empty
+    }
+
+  val onesUsingUnfold:Stream[Int] = unfold(1)(_ => Some(1, 1))
+
+  def constantUsingUnfold[A](a:A): Stream[A] = unfold(a)(s => Some(s, s))
+
+  def fromUsingUnfold(n: Int): Stream[Int] = 
+    unfold(n)(s => Some(s, s+1))
+
+  def fibsnUsingUnfold(a:Int, b:Int):Stream[Int] =
+    unfold((a,b))(xy => Some(xy._1 + xy._2, (xy._2, xy._1+xy._2)))
+
+  val fibsUsingUnfold:Stream[Int] = 
+    Stream.cons(0, Stream.cons(1, fibsnUsingUnfold(0, 1)))
+
 }
